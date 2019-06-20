5368973e9f85c12507011e3a0ef9ebf9528e11fc
nprofilovic <nprofilovic@gmail.com>
2019-06-20 12:43:05 +0200

    Make Icon and Footer components

--- src/Components/Header_footer/Header.js
+++ src/Components/Header_footer/Header.js
@@ -4,7 +4,7 @@ import ToolBar from '@material-ui/core/ToolBar';
 import Button from '@material-ui/core/Button';
 
 import { Link } from 'react-router-dom';
-
+import { CityLogo } from '../ui/icons';
 
 
 class Header extends Component {
@@ -14,7 +14,7 @@ class Header extends Component {
                 <ToolBar style={{display:'flex'}}>
                     <div style={{flexGrow: 1}}>
                         <div className="header_logo">
-                             Logo
+                            <CityLogo link={true} linkTo="/" width="70px" height="70px" />
                         </div>
                     </div>
                     <Link to="/the_team">

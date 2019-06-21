d48634b2ec7b5f613ad5daafb67978a2da40f5a1
nprofilovic <nprofilovic@gmail.com>
2019-06-21 12:54:27 +0200

    Finish Match block component

--- src/Components/Home/matches/Blocks.js
+++ src/Components/Home/matches/Blocks.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import {firebaseMatches} from '../../../firebase';
 import {firebaseLooper, reverseArray} from '../../ui/misc';
+import MatchesBlock from '../../ui/matches_block';
+import Slide from 'react-reveal/Slide';
 
 
 class Blocks extends Component {
@@ -10,7 +12,7 @@ class Blocks extends Component {
     }
     
     componentDidMount(){
-        firebaseMatches.limitToLast(6).once('value').then((snapshot) => {
+        firebaseMatches.limitToLast(7).once('value').then((snapshot) => {
             const matches = firebaseLooper(snapshot);
            
             this.setState({
@@ -19,10 +21,18 @@ class Blocks extends Component {
             
         })
     }
-    showMatches = () => (
-        <div>
-            Match
-        </div>
+    showMatches = (matches) => (
+        matches ?
+            matches.map((match) => (
+                <Slide bottom key={match.id}>
+                    <div className="item">
+                        <div className="wrapper">
+                            <MatchesBlock match = {match} />
+                        </div>
+                    </div> 
+                </Slide>
+            ))
+        :null
     )
 
     render() {

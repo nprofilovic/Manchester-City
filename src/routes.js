ec7b8b9a4563fda3f5eb784a2c383d58232cdf6f
nprofilovic <nprofilovic@gmail.com>
2019-06-23 16:04:21 +0200

    Make Admin Match component

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Layout from './Hoc/Layout';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Home from './Components/Home';
 import SignIn from './Components/signin';
 import Dashboard from './Components/admin/Dashboard';
 import PrivateRoutes from './Components/authRoutes/privateRoutes';
 import PublicRoutes from './Components/authRoutes/publicRoutes';
-
+import AdminMatches from './Components/admin/matches';
 
 
 const Routes = (props) => {
@@ -14,6 +14,7 @@ const Routes = (props) => {
         <Layout>
             <Switch>
                 <PrivateRoutes {...props} path="/dashboard" exact component={Dashboard} />
+                <PrivateRoutes {...props} path="/admin_matches" exact component={AdminMatches} />
                 <PublicRoutes {...props } restricted={false} exact component={Home} path="/" />
                 <PublicRoutes {...props } restricted={true} exact component={SignIn} path="/sign_in" />
             </Switch>

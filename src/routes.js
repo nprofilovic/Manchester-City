135bcf9897e66e3d85d223fb902222ae5df7f4d9
nprofilovic <nprofilovic@gmail.com>
2019-06-23 14:48:02 +0200

    Make Private and Public Route

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,18 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import SignIn from './Components/signin';
 import Dashboard from './Components/admin/Dashboard';
+import PrivateRoutes from './Components/authRoutes/privateRoutes';
+import PublicRoutes from './Components/authRoutes/publicRoutes';
+
 
 
 const Routes = (props) => {
     return (
         <Layout>
             <Switch>
-                <Route exact component={SignIn} path="/sign_in" />
-                <Route exact component={Home} path="/" />
-                <Route exact component={Dashboard} path="/dashboard" />
+                <PrivateRoutes {...props} path="/dashboard" exact component={Dashboard} />
+                <PublicRoutes {...props } restricted={false} exact component={Home} path="/" />
+                <PublicRoutes {...props } restricted={true} exact component={SignIn} path="/sign_in" />
             </Switch>
         </Layout>
     )

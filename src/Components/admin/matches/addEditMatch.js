36f51242828d27112c5ab8731ac5e572faf7e0ea
nprofilovic <nprofilovic@gmail.com>
2019-06-23 18:46:59 +0200

    Finis Edit match on firebase

--- src/Components/admin/matches/addEditMatch.js
+++ src/Components/admin/matches/addEditMatch.js
@@ -214,6 +214,54 @@ class AddEditMatch extends Component {
           });
     }
 
+    successForm(message) {
+        this.setState({ formSuccess: message });
+        setTimeout(()=>{
+            this.setState({ formSuccess: '' }); 
+        }, 2000)
+    }
+
+    submitForm(e) {
+        e.preventDefault();
+
+        let dataToSubmit = {};
+        let formIsValid = true;
+
+        for(let key in this.state.formdata){
+            dataToSubmit[key] = this.state.formdata[key].value;
+            formIsValid = this.state.formdata[key].valid && formIsValid;
+        }
+        this.state.teams.forEach((team)=>{
+            if(team.shortName === dataToSubmit.local){
+                dataToSubmit['localThmb'] = team.thmb
+            }
+            if(team.shortName === dataToSubmit.away){
+                dataToSubmit['awayThmb'] = team.thmb
+            }
+
+        })
+        if(formIsValid) {
+            if(this.state.formType === 'Edit Match'){
+                firebaseDB.ref(`matches/${this.state.matchId}`)
+                .update(dataToSubmit).then(()=>{
+                    this.successForm('Update correctly')
+                }).catch((e)=> {
+                    this.setState({ formError: true });
+                })
+            } else {
+                // add match
+            }
+            
+            
+
+        } else {
+            this.setState({ formError: true });
+            
+        }
+        
+        
+    }
+
     componentDidMount() {
         const matchId = this.props.match.params.id;
         const getTeams = (match, type) => {
